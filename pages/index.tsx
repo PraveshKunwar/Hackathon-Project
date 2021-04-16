675021b0ea0d8805e6be513a90cababc6327e3f1
Pravesh Kunwar <praveshk04@outlook.com>
2021-04-15 22:12:28 -0400

    (feat): Initial commits.

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+    </div>
+  );
+}
