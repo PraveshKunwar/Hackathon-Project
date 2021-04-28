6f802e108297dfa30e5bef4e5e52b1f4a6cc1819
Pravesh Kunwar <praveshk04@outlook.com>
2021-04-27 21:47:28 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import AuthComponent from "../util/firebase.config";
 import Profile from "../components/Profile";
 import { useEffect } from "react";
 import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Index: NextPage = () => {
   const { user, loading } = useAuth();
@@ -43,6 +44,16 @@ const Index: NextPage = () => {
           href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+        />
+        <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
+
+        <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
+
+        <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"></script>
       </Head>
       {user ? <Profile /> : <AuthComponent />}
     </div>
