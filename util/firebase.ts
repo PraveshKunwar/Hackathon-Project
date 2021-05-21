6fff537e91b4aade5446b563e36039371eb06532
Pravesh Kunwar <praveshk04@outlook.com>
2021-05-20 20:39:28 -0400

    Update firebase.ts

--- util/firebase.ts
+++ util/firebase.ts
@@ -2,14 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyBEctGsLYCLQXQF2Se4w5V_F1iELR2Z9I0",
-  projectId: "hackathon-cba40",
-  authDomain: "hackathon-cba40.firebaseapp.com",
-  databaseURL: "https://hackathon-cba40-default-rtdb.firebaseio.com",
-  storageBucket: "hackathon-cba40.appspot.com",
-  messagingSenderId: "1072231438620",
-  appId: "1:1072231438620:web:22ed9bbfae0a324aa102d9",
-  measurementId: "G-D50FQNDVMJ",
+   apiKey: "AIzaSyDSHi3DZfaHQ8NufG8fMvelbzUrrC6t-HE",
+    authDomain: "hackathon2-901e8.firebaseapp.com",
+    projectId: "hackathon2-901e8",
+    storageBucket: "hackathon2-901e8.appspot.com",
+    messagingSenderId: "732442230061",
+    appId: "1:732442230061:web:db09c11fa3519ffcdbe7a4",
+    measurementId: "G-SLRCBX2V5E",
 };
 
 const initFirebase = () => {
